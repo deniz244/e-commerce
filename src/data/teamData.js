4cbd3b5e98cd62bb5c39b0c0cd759cd1efd0e98f
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 09:43:15 +0300

    mock team data created images added in assets

---
+++ src/data/teamData.js
@@ -0,0 +1,30 @@
+import mem1 from "../assets/aboutPage/team/team-1-user-1.jpg";
+import mem2 from "../assets/aboutPage/team/team-1-user-2.jpg";
+import mem3 from "../assets/aboutPage/team/team-1-user-3.jpg";
+
+export const teamData = [
+  {
+    img: mem1,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+  {
+    img: mem2,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+  {
+    img: mem3,
+    header: "username",
+    role: "professsion",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+];
