98bcfbd7992bf54e8ab1097a36e27c445e02bc90
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 14:24:02 +0300

    ProductDecription needs css fixes

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../layout/Header";
 import ProductCard from "../components/productPageComps/ProductCard";
+import ProductDescription from "../components/productPageComps/ProductDescription";
 
 export default function ProductPage() {
   return (
     <div>
       <Header />
       <ProductCard />
+      <ProductDescription />
     </div>
   );
 }
