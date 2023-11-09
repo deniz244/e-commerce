b87ce6e46f119123745e11f09497d1f8b9d6c3de
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 09:47:17 +0300

    AboutTeam completed

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutHeader from "../components/aboutPageComps/AboutHeader";
+import AboutTeam from "../components/aboutPageComps/AboutTeam";
 
 export default function AboutPage() {
   return (
     <div>
       <AboutHeader />
+      <AboutTeam />
     </div>
   );
 }
