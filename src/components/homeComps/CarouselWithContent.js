bbbd6c07ce10d3fe3260925e1d7b79700a07af91
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 20:29:20 +0300

    Carousel fixed

--- src/components/homeComps/CarouselWithContent.js
+++ src/components/homeComps/CarouselWithContent.js
@@ -1,78 +1,65 @@
-import React from "react";
 import { Carousel, Typography, Button } from "@material-tailwind/react";
-//import { Link } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import bg from "../../assets/shop-hero-1-product-slide-1.jpg";
 export default function CarouselWithContent() {
   return (
-    <Carousel className="rounded-xl">
+    <Carousel className="rounded h-[716px] ">
       <div className="relative h-full w-full">
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-          <div className="w-3/4 text-center md:w-2/4">
+        <img src={bg} alt="image 1" className=" h-full w-full object-cover" />
+        <div className="absolute inset-0 grid place-items-center  ">
+          <div className="w-3/4 text-center md:w-2/4  flex flex-col gap-5">
             <Typography
               variant="h1"
               color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              className="font-bold sm:text-6xl text-[40px] text-white text-center"
             >
-              The Beauty of Nature
+              GROCERIES DELIVERY
             </Typography>
             <Typography
               variant="lead"
               color="white"
-              className="mb-12 opacity-80"
+              className="w-[290px] sm:w-1/2 m-auto font-normal text-center text-white text-xl"
             >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that quality
-              of air that emanation from old trees, that so wonderfully changes
-              and renews a weary spirit.
+              We know how large objects will act, but things on a small scale
+              just do not act that way.
             </Typography>
             <div className="flex justify-center gap-2">
-              <Button size="lg" color="white">
-                Explore
-              </Button>
-              <Button size="lg" color="white" variant="text">
-                Gallery
-              </Button>
+              <Link
+                to="/shopping"
+                className="w-40 m-auto rounded py-[1rem] px-10  bg-primary-color font-bold text-base text-center text-white"
+              >
+                Start Now
+              </Link>
             </div>
           </div>
         </div>
       </div>
       <div className="relative h-full w-full">
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
-          <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
+        <img src={bg} alt="image 1" className="h-full w-full object-cover" />
+        <div className="absolute inset-0 grid place-items-center bg-black/75 ">
+          <div className="w-3/4 text-center md:w-2/4  flex flex-col gap-5">
             <Typography
               variant="h1"
               color="white"
-              className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+              className="font-bold sm:text-6xl text-[40px] text-white text-center"
             >
-              The Beauty of Nature
+              GROCERIES DELIVERY
             </Typography>
             <Typography
               variant="lead"
               color="white"
-              className="mb-12 opacity-80"
+              className="w-[290px] sm:w-1/2 m-auto font-normal text-center text-white text-xl"
             >
-              It is not so much for its beauty that the forest makes a claim
-              upon men&apos;s hearts, as for that subtle something, that quality
-              of air that emanation from old trees, that so wonderfully changes
-              and renews a weary spirit.
+              We know how large objects will act, but things on a small scale
+              just do not act that way.
             </Typography>
-            <div className="flex gap-2">
-              <Button size="lg" color="white">
-                Explore
-              </Button>
-              <Button size="lg" color="white" variant="text">
-                Gallery
-              </Button>
+            <div className="flex justify-center gap-2">
+              <Link
+                to="/product-list"
+                className="w-40 m-auto rounded py-[1rem] px-10  bg-primary-color font-bold text-base text-center text-white"
+              >
+                Start Now
+              </Link>
             </div>
           </div>
         </div>
