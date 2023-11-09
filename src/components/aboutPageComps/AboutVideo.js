320821f520e3c594f4714e6f93b569e3e6a3b589
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 10:26:20 +0300

    comment added

--- src/components/aboutPageComps/AboutVideo.js
+++ src/components/aboutPageComps/AboutVideo.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function AboutVideo() {
   return (
     <div className="relative z-0 py-20 ml-96 ">
+      {/*FARKLI VİDEO KONULUNCA ÇALIŞMIYOR */}
       <iframe
         className="w-[989px] h-[540px] rounded"
         src="https://www.youtube.com/embed/tgbNymZ7vqY"
