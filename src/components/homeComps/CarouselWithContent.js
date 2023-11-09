4d181a7e1254d2cc11498d92e0fb69b7652ee71d
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-09 20:43:36 +0300

    CarouselWithContent needs css fixes

--- src/components/homeComps/CarouselWithContent.js
+++ src/components/homeComps/CarouselWithContent.js
@@ -6,14 +6,21 @@ export default function CarouselWithContent() {
     <Carousel className="rounded h-[716px] ">
       <div className="relative h-full w-full">
         <img src={bg} alt="image 1" className=" h-full w-full object-cover" />
-        <div className="absolute inset-0 grid place-items-center  ">
-          <div className="w-3/4 text-center md:w-2/4  flex flex-col gap-5">
+        <div className="absolute inset-0 grid place-items-center">
+          <div className="w-3/4 text-center md:w-2/4  flex flex-col gap-[35px]">
+            <Typography
+              variant="h5"
+              color="white"
+              className="w-[290px] sm:w-1/2 m-auto font-normal text-center text-white text-[16px] leading-[24px] tracking-[0.1px]"
+            >
+              SUMMER 2020
+            </Typography>
             <Typography
               variant="h1"
               color="white"
               className="font-bold sm:text-6xl text-[40px] text-white text-center"
             >
-              GROCERIES DELIVERY
+              NEW COLLECTION
             </Typography>
             <Typography
               variant="lead"
@@ -26,9 +33,9 @@ export default function CarouselWithContent() {
             <div className="flex justify-center gap-2">
               <Link
                 to="/shopping"
-                className="w-40 m-auto rounded py-[1rem] px-10  bg-primary-color font-bold text-base text-center text-white"
+                className="w-40 m-auto rounded py-[1rem] px-10  bg-[#2DC071] font-bold text-base text-center text-white"
               >
-                Start Now
+                SHOP NOW
               </Link>
             </div>
           </div>
@@ -36,14 +43,14 @@ export default function CarouselWithContent() {
       </div>
       <div className="relative h-full w-full">
         <img src={bg} alt="image 1" className="h-full w-full object-cover" />
-        <div className="absolute inset-0 grid place-items-center bg-black/75 ">
+        <div className="absolute inset-0 grid place-items-center">
           <div className="w-3/4 text-center md:w-2/4  flex flex-col gap-5">
             <Typography
               variant="h1"
               color="white"
               className="font-bold sm:text-6xl text-[40px] text-white text-center"
             >
-              GROCERIES DELIVERY
+              NEW COLLECTION
             </Typography>
             <Typography
               variant="lead"
@@ -56,7 +63,7 @@ export default function CarouselWithContent() {
             <div className="flex justify-center gap-2">
               <Link
                 to="/product-list"
-                className="w-40 m-auto rounded py-[1rem] px-10  bg-primary-color font-bold text-base text-center text-white"
+                className="w-40 m-auto rounded py-[1rem] px-10  bg-[#2DC071] font-bold text-base text-center text-white"
               >
                 Start Now
               </Link>
