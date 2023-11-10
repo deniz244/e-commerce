0e15c37497bc004188806c85fc0ab37992bd6bbf
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-10 14:15:23 +0300

    pic change

--- src/components/productPageComps/ProductCard.js
+++ src/components/productPageComps/ProductCard.js
@@ -41,7 +41,7 @@ export default function ProductCard() {
                   className="opacity-50 w-24 h-[75px]"
                 ></img>
                 <img
-                  src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                  src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
                   alt="image 1"
                   className="opacity-50 w-24 h-[75px]"
                 ></img>
